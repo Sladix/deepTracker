ab219a98f37bff93ca8a88a3a7882d31ea83602c
Antoine Guillien <aguillien@lafourchette.com>
2018-06-13 14:17:05 +0200

    dashoard tab open

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,43 @@
+let dashboardOpen = false;
+const actionsHandler = {
+    'openDashboard' : openDashBoard,
+    'closeDashBoard' : closeDashBoard
+};
+
+
+browser.extension.onConnect.addListener(function(port) {
+    console.log("Connected .....");
+
+    port.onMessage.addListener(function(msg) {
+        console.log(`Message recieved : ${msg}`);
+        if (actionsHandler[msg.action]){
+            actionsHandler[msg.action]();
+        }
+    });
+});
+
 browser.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion)
-})
+    console.log('previousVersion', details.previousVersion)
+});
 
-browser.browserAction.setBadgeText({
-  text: `'Allo`
-})
+// browser.browserAction.setBadgeText({
+//     text: `'Allo`
+// });
 
-console.log(`'Allo 'Allo! Event Page for Browser Action`)
+// Open dashboard
+function openDashBoard() {
+    if(!dashboardOpen){
+        browser.tabs.create(
+            {
+                url: browser.runtime.getURL('dashboard.html')
+            }
+        );
+        dashboardOpen = true;
+    }
+};
+
+function closeDashBoard(){
+    dashboardOpen = false;
+}
+
+console.log(`'Allo 'Allo! Event Page for Browser Action`);
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,14 +1,19 @@
 (() => {
     const dashBtn = document.querySelector('#dash-btn');
+    const port = browser.extension.connect({
+        name: "deep-communication"
+    });
 
+    // Dashboard open
     dashBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        chrome.tabs.create(
-            {
-                url: chrome.runtime.getURL('dashboard.html')
-            }, (r) => {
-                console.log(r);
-            }
-        );
+        port.postMessage({ action : 'openDashboard'});
+        console.log('message sent');
     }, false);
+
+
+
+    port.onMessage.addListener(function(msg) {
+        console.log("message recieved" + msg);
+    });
 })();
